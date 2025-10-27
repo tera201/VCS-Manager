31421ebda895b93b63e3b01fe1a3d1f5a51dd8cc
rnaryshkin <r.naryshkin99@gmail.com>
2025-10-27 21:05:19 +0000

    Added gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    alias(libs.plugins.kotlin)
 }
 
 java {
@@ -13,28 +13,17 @@ repositories {
     mavenCentral()
 }
 
-val commonsVersion = "2.14.0"
-val lombokVersion = "1.18.30"
-val lang3Version = "3.3.2"
-val slf4jVersion = "1.7.36"
-val jgitVersion = "7.1.0.202411261347-r"
-val sqliteVersion = "3.49.1.0"
-val jacksonVersion = "2.15.1"
-val coroutineVersion = "1.10.1"
-val exposedVersion = "1.0.0-rc-2"
-
 dependencies {
-    implementation("commons-io:commons-io:$commonsVersion")
-    compileOnly("org.projectlombok:lombok:$lombokVersion")
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
-    implementation("org.apache.commons:commons-lang3:$lang3Version")
-    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:$jgitVersion"){
+    implementation(libs.commons.io)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.commons.lang3)
+    implementation(libs.slf4j.simple)
+    implementation(libs.jgit) {
         exclude(group = "org.slf4j")
     }
-    implementation("org.xerial:sqlite-jdbc:$sqliteVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:${exposedVersion}")
+    implementation(libs.sqlite)
+    implementation(libs.jackson.databind)
+    implementation(libs.coroutines.core)
+    implementation(libs.bundles.exposed)
 }
